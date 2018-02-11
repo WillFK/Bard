20a4ca0b07670b5abbb867d0d847818859a011b9
William <williamferk@gmail.com>
2018-02-11 21:01:04 +0100

    silly script

---
+++ main.js
@@ -0,0 +1,83 @@
+(function() {
+    var DEFAULT_LIMIT = 10
+    var limit = null
+    var region = null
+    var type = null
+    const cheerio = require('cheerio')
+    const minimalist = require('minimist')
+    const got = require('got')
+    const Rx = require('rxjs/Rx')
+
+    processArguments()
+
+    Rx.Observable.from(Rx.Observable.range(1, 50))
+        .flatMap(readMatches)
+        .map(address => "http://www.lolskill.net/" + address)
+        .flatMap(readMatch)
+        .filter(filterRegion)
+        .filter(filterType)
+        .take(limit)
+        .subscribe((match) => {
+                console.log(JSON.stringify(match, null, 4))
+            }, (err) => {
+                console.err(err);
+            }, () => {
+                process.exit() // workaround
+            })
+
+    function filterRegion(match) {
+        if (region) {
+            return match.region.toLowerCase().indexOf(region.toLowerCase()) >= 0
+        }  else {
+            return true
+        }
+    }
+
+    function filterType(match) {
+        if (type) {
+            return match.type.toLowerCase().indexOf(type.toLowerCase()) >= 0
+        }  else {
+            return true
+        }
+    }
+
+    function readMatches(index) {
+        return Rx.Observable.create(function(observable) {
+            got('http://www.lolskill.net/live-matches/page-'+index, { json: false }).then(response => {
+                const $ = cheerio.load(response.body)
+                $('.live-match').find('a').each(function(index, it) {
+                    observable.next($(it).attr('href'))
+                  })
+                observable.complete()
+            }).catch(error => {
+                observable.error(error)
+            });
+        })
+    }
+
+    function readMatch(url) {
+        return Rx.Observable.create(function(observable) {
+            got(url, { json: false }).then(response => {
+                const $ = cheerio.load(response.body)
+                var type = $('.row.gametype h3')
+                    .map(function() { return $(this).text().trim() })
+                    .get().join(' / ')
+                var region =  $('#search-realm').text().trim()
+                var summoners = $('.gamesummoner-card .summoner-name>a').map(function(index, it) {
+                    return $(it).html()
+                  }).get()
+                observable.next({ type: type, region: region, summoners: summoners })
+                observable.complete()
+            }).catch(error => {
+                observable.error(error)
+            });
+        })
+    }
+
+    function processArguments() {
+        var args = minimalist(process.argv.slice(2))
+        limit = args['l'] || args['limit'] || DEFAULT_LIMIT
+        region = args['r'] || args['region']
+        type = args['t'] || args['type']
+    }
+})()
